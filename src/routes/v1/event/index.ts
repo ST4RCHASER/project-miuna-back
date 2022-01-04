b47cfbb63fb6b41f9c684cd17bfa381824032b31
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-01-04 17:28:03 +0700

    Update index.ts

--- src/routes/v1/event/index.ts
+++ src/routes/v1/event/index.ts
@@ -1,12 +1,15 @@
 import list from './list'
 import create from './create'
+import deleteR from './delete'
 import express from 'express'
 import scan from './scan'
 const router = express.Router()
 
 router.use('/list', list)
 router.use('/create', create)
+router.use('/delete', deleteR)
 router.use('/scan', scan)
+
 export default router
 
 
