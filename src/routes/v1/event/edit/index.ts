c081e170d62690f4a72589c49e89f2eacf98eca3
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-01-04 21:01:38 +0700

    try catch

--- src/routes/v1/event/edit/index.ts
+++ src/routes/v1/event/edit/index.ts
@@ -2,56 +2,66 @@ import express from 'express'
 const router = express.Router()
 import { RESTResp, requireAuth, Event, findUserEventRecordByEventID, MinuRequest, getEventByID, EventState, getEventRecordByID, leaveUserFromEvent, joinUserToEvent, ModelType } from '@project-miuna/utils'
 router.put('/:id', requireAuth, async (_, res) => {
-    let req: MinuRequest = _ as any;
-    let event: Event = await getEventByID(req.db, req.params.id);
-    let body = req.body;
-    if (!body || !body.name || !body.time || !body.time.start || !body.time.end || !req.params.id) {
-        console.log(body);
+    try {
+        let req: MinuRequest = _ as any;
+        let event: Event = await getEventByID(req.db, req.params.id);
+        let body = req.body;
+        if (!body || !body.name || !body.time || !body.time.start || !body.time.end || !req.params.id) {
+            console.log(body);
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 409,
+                message: 'missing some field',
+            }
+            return res.status(409).send(response);
+        }
+        if (!event) {
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 404,
+                message: 'event not found',
+            }
+            return res.status(404).send(response);
+        }
+        if (event.state == EventState.DELETED) {
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 403,
+                message: 'event not found',
+            }
+            return res.status(409).send(response);
+        }
+        if (event.ownerID != req.user.id) {
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 409,
+                message: 'you are not the owner of this event',
+            }
+            return res.status(409).send(response);
+        }
+        await req.db.getModel(ModelType.EVENT).findByIdAndUpdate(req.params.id, {
+            name: body.name || 'unnamed event',
+            ownerID: req.user.id,
+            time: {
+                created: new Date().getTime().toString(),
+                start: body.time.start,
+                end: body.time.end
+            }
+        }, { new: true });
         const response: RESTResp<never> = {
-            success: false,
-            statusCode: 409,
-            message: 'missing some field',
+            success: true,
+            statusCode: 200,
+            message: 'event updated',
         }
-        return res.status(409).send(response);
+        return res.status(200).send(response);
+    } catch (e) {
+        return res.status(500).send(
+            {
+                success: false,
+                statusCode: 500,
+                message: 'internal server error',
+            }
+        );
     }
-    if (!event) {
-        const response: RESTResp<never> = {
-            success: false,
-            statusCode: 404,
-            message: 'event not found',
-        }
-        return res.status(404).send(response);
-    }
-    if (event.state == EventState.DELETED) {
-        const response: RESTResp<never> = {
-            success: false,
-            statusCode: 403,
-            message: 'event not found',
-        }
-        return res.status(409).send(response);
-    }
-    if (event.ownerID != req.user.id) {
-        const response: RESTResp<never> = {
-            success: false,
-            statusCode: 409,
-            message: 'you are not the owner of this event',
-        }
-        return res.status(409).send(response);
-    }
-    await req.db.getModel(ModelType.EVENT).findByIdAndUpdate(req.params.id, {
-        name: body.name || 'unnamed event',
-        ownerID: req.user.id,
-        time: {
-            created: new Date().getTime().toString(),
-            start: body.time.start,
-            end: body.time.end
-        }
-    }, { new: true });
-    const response: RESTResp<never> = {
-        success: true,
-        statusCode: 200,
-        message: 'event updated',
-    }
-    return res.status(200).send(response);
 });
 export default router
--- src/routes/v1/event/info/index.ts
+++ src/routes/v1/event/info/index.ts
@@ -2,31 +2,41 @@ import express from 'express'
 const router = express.Router()
 import { RESTResp, requireAuth, Event, findUserEventRecordByEventID, MinuRequest, getEventByID, EventState, getEventRecordByID, leaveUserFromEvent, joinUserToEvent, ModelType } from '@project-miuna/utils'
 router.get('/:id', requireAuth, async (_, res) => {
-    let req: MinuRequest = _ as any;
-    let event: Event = await getEventByID(req.db, req.params.id);
-    if (!event) {
-        const response: RESTResp<never> = {
-            success: false,
-            statusCode: 404,
-            message: 'event not found',
+    try {
+        let req: MinuRequest = _ as any;
+        let event: Event = await getEventByID(req.db, req.params.id);
+        if (!event) {
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 404,
+                message: 'event not found',
+            }
+            return res.status(404).send(response);
         }
-        return res.status(404).send(response);
-    }
-    if (event.state == EventState.DELETED) {
-        const response: RESTResp<never> = {
-            success: false,
-            statusCode: 403,
-            message: 'event not found',
+        if (event.state == EventState.DELETED) {
+            const response: RESTResp<never> = {
+                success: false,
+                statusCode: 403,
+                message: 'event not found',
+            }
+            return res.status(403).send(response);
         }
-        return res.status(403).send(response);
+        //send event infomation
+        const response: RESTResp<object> = {
+            success: true,
+            statusCode: 200,
+            message: 'event found',
+            content: event
+        }
+        return res.status(200).send(response);
+    } catch (e) {
+        return res.status(500).send(
+            {
+                success: false,
+                statusCode: 500,
+                message: 'internal server error',
+            }
+        );
     }
-    //send event infomation
-    const response: RESTResp<object> = {
-        success: true,
-        statusCode: 200,
-        message: 'event found',
-        content: event
-    }
-    return res.status(200).send(response);
 });
 export default router
